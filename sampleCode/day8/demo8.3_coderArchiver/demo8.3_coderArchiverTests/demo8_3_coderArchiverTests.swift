598b4ed0338ef8af9ff2e69708358f404cabc1a9
iap <al@alswebdesigns.net>
2015-08-14 13:33:31 -0700

    Folder structure was too flat and cluttered. Material now grouped by day# (i.e., lecture #)

---
+++ sampleCode/day8/demo8.3_coderArchiver/demo8.3_coderArchiverTests/demo8_3_coderArchiverTests.swift
@@ -0,0 +1,36 @@
+//
+//  demo8_3_coderArchiverTests.swift
+//  demo8.3_coderArchiverTests
+//
+//  Created by Al on 8/14/15.
+//  Copyright (c) 2015 Al. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class demo8_3_coderArchiverTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
